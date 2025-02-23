62eaa1b2b3d23f3aaf121dd71cdf939fa5a2d15e
Varun Gotmare <varunpgotmare@gmail.com>
2025-02-23 22:32:21 +0530

    frontend vite setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className=''>
+        <text className='text-2xl '>Hello Vite + React!</text>
+      </div>
+    </>
+  )
+}
+
+export default App
